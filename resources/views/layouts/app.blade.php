b1a51367a8c5ed51dcbc35c93153bb46ead1e660
Fredy Palacios <fredy_palacios_online@hotmail.com>
2025-05-02 16:10:13 +0200

    Added AdminLTE 3 and applied it to login and register views
    Added AdminLTE 3 to improve the design of login and register pages; set up MySQL database
    
    The login and register pages now have a better look using AdminLTE 3.
    Also connected the project to a MySQL database for future data storage.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('vendor/adminlte/plugins/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
